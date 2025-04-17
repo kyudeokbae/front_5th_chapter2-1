a90e34f7bb7f362ea38c8866a13d14f286de7673
배규덕 <mandarinduk@gmail.com>
2025-04-18 01:18:24 +0900

    기본과제 배포

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   test: {
@@ -7,4 +11,13 @@ export default defineConfig({
     setupFiles: 'src/setupTests.js',
   },
   base: '/front_5th_chapter2-1/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        advanced: resolve(__dirname, 'index.advanced.html'),
+        basic: resolve(__dirname, 'index.basic.html'),
+      },
+    },
+  },
 });
